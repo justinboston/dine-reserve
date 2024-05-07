d6e6fb2b36f340a848456542aef1ad646d9ea696
Justin Boston <justin23boston@gmail.com>
2024-05-07 18:47:39 -0400

    frontend changes

--- server-temp.js
+++ server-temp.js
@@ -35,9 +35,9 @@ app.get("/restaurants", (req, res) => {
     res.render("restaurants", {
         logged_in: false,
         restaurants: [
-            { name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg' },
+            { name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: './photo.png'},
             { name: 'Quick Bites', type: 'Fast Food', address: '456 Elm St', image: 'image_url2.jpg' },
-            { name: 'Sushi World', type: 'Japanese', address: '789 Pine St', image: 'image_url3.jpg' },
+            { name: 'Sushi World', type: 'Japanese', address: '789 Pine St', image: 'https://d2sygdwsqncsjz.cloudfront.net/364245.jpg' },
             { name: 'Pasta House', type: 'Italian', address: '321 Maple St', image: 'image_url4.jpg' },
             { name: 'Café Delight', type: 'Café', address: '654 Oak St', image: 'image_url5.jpg' }
         ]
@@ -51,27 +51,17 @@ app.get("/reservations", (req, res) => {
         reservations: [
             {
                 date: new Date('2024-05-01'), time: '19:00', guests: 2, restaurant: {
-                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg'
-                }
-            },
-            {
-                date: new Date('2024-05-03'), time: '12:00', guests: 4, restaurant: {
-                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg'
+                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'https://d2sygdwsqncsjz.cloudfront.net/364245.jpg'
                 }
             },
             {
                 date: new Date('2024-05-04'), time: '18:00', guests: 3, restaurant: {
-                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg'
+                    name: 'Pasta House', type: 'Italian', address: '321 Maple St', image: 'https://d2sygdwsqncsjz.cloudfront.net/364245.jpg'
                 }
             },
             {
                 date: new Date('2024-05-05'), time: '20:00', guests: 1, restaurant: {
-                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg'
-                }
-            },
-            {
-                date: new Date('2024-05-06'), time: '15:00', guests: 2, restaurant: {
-                    name: 'The Gourmet Place', type: 'Fine Dining', address: '123 Main St', image: 'image_url1.jpg'
+                    name: 'Sushi World, type: 'Japanese', address: '789 Pine St', image: 'https://media.timeout.com/images/106013795/750/422/image.jpg'
                 }
             },
         ]
